6d1b548cdf0ffc332581569112a4bd74100b5c79
Mikecooper49 <mikecooper49@gmail.com>
2019-02-17 17:29:23 +0000

    13th Feb

---
+++ Firstphp/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 2019-02-17
+ * Time: 17:26
+ */
+
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+<p>
+<?php
+echo "Hello World";
+?>
+</p>
+</body>
+</html>
+
